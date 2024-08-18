a6e4ae496c7f9ed5f9ada3ad37d101d8f9835884
kevintsai <kevintsai@TD-ID10-01460.mshome.net>
2024-08-18 11:48:38 +0800

    first release

---
+++ src/main/java/com/kt/web/SpringBootWeb3Application.java
@@ -0,0 +1,13 @@
+package com.kt.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/web/SpringBootWeb3ApplicationTests.java
@@ -0,0 +1,21 @@
+package com.kt.web;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeb3ApplicationTests {
+
+	@Test
+	void contextLoads1() {
+		System.out.println("TEST1");
+	}
+
+
+	@Test
+	void contextLoads2() {
+		System.out.println("TEST2");
+	}
+}
